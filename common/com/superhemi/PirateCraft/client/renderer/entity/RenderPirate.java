73c33d70dab44f9ea9c311fafbd7ad787d2275ce
superhemi <hemisuperhemi@yahoo.com>
2013-12-13 17:14:58 -0500

    update

---
+++ common/com/superhemi/PirateCraft/client/renderer/entity/RenderPirate.java
@@ -0,0 +1,17 @@
+package com.superhemi.PirateCraft.client.renderer.entity;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderPirate extends RenderLiving
+{
+
+	private static final ResourceLocation y  = new ResourceLocation("");
+	 
+	public RenderPirate(ModelBase par1ModelBase, float par2) {
+		super(par1ModelBase, par2);
+
+	}
+
+}
--- common/com/superhemi/PirateCraft/entity/EntityPirate.java
+++ common/com/superhemi/PirateCraft/entity/EntityPirate.java
@@ -1,5 +1,6 @@
 package com.superhemi.PirateCraft.entity;
 
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.world.World;
 
@@ -9,5 +10,40 @@ public class EntityPirate extends EntityMob{
 		super(par1World);
         this.setSize(0.9f, 1.3f);
 	}
+    
+	@Override
+	protected void applyEntityAttributes()
+	{
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setAttribute(10.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setAttribute(0.20000000298023224D);
+	}
+	
+	@Override
+	protected String getLivingSound()
+	{
+		return null;
+		
+	}
+	
+	@Override
+    protected String getHurtSound()
+    {
+		return null;
+        
+    }
 
+    @Override
+    protected String getDeathSound()
+    {
+		return null;
+        
+    }
+
+    @Override
+    protected float getSoundVolume()
+    {
+        return 0.4F;
+    }
 }
+
