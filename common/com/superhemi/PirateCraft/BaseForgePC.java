a92423f26ed4fa74cf6cedaf4dbb27fc4862b360
superhemi <hemisuperhemi@yahoo.com>
2013-12-13 05:35:43 -0500

    setting up base files and breaking things *will be taking a break to see about setting up auto commits

---
+++ common/com/superhemi/PirateCraft/BaseForgePC.java
@@ -0,0 +1,18 @@
+package com.superhemi.PirateCraft;
+
+import com.superhemi.PirateCraft.lib.Reference;
+/**
+ * PirateCraft
+ * 
+ * BaseForgePC
+ * 
+ * @author superhemi
+ *@license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION_NUMBER)
+@NetworkMod(clientSideRequired = true, serverSideRequired = false, versionBounds = "0.0.1")
+public class BaseForgePC {
+
+}
---
+++ common/com/superhemi/PirateCraft/lib/Reference.java
@@ -0,0 +1,18 @@
+package com.superhemi.PirateCraft.lib;
+
+/**
+ * PirateCraft
+ * 
+ * Reference
+ * 
+ * @author superhemi
+ *@license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
+
+public class Reference {
+
+	public static final String MOD_ID = "PiraCraft";
+	public static final String MOD_NAME = "PirateCraft";
+	public static final String VERSION_NUMBER = "@VERSION@ (build @BUILD_NUMBER@";
+}
