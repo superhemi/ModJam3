fadc3fd5d2717a7e69b4ab0e94636c05f85d2a05
superhemi <hemisuperhemi@yahoo.com>
2013-12-13 06:23:51 -0500

    update

--- common/com/superhemi/PirateCraft/lib/Reference.java
+++ common/com/superhemi/PirateCraft/lib/Reference.java
@@ -15,6 +15,7 @@ public class Reference {
 	public static final String MOD_ID = "PiraCraft";
 	public static final String MOD_NAME = "PirateCraft";
 	public static final String VERSION_NUMBER = "0.0.1";
+    public static final String CHANNEL_NAME = MOD_ID;
 	public static final String SERVER_PROXY_CLASS = "com.superhemi.PirateCraft.core.proxy.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "com.superhemi.PirateCraft.core.proxy.ClientProxy";
 }
