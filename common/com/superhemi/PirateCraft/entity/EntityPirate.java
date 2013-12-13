6ab366f9b024b23391368a56d2b669bad5f6073d
superhemi <hemisuperhemi@yahoo.com>
2013-12-13 17:01:02 -0500

    upgrade

---
+++ common/com/superhemi/PirateCraft/entity/EntityPirate.java
@@ -0,0 +1,13 @@
+package com.superhemi.PirateCraft.entity;
+
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.world.World;
+
+public class EntityPirate extends EntityMob{
+
+	public EntityPirate(World par1World) {
+		super(par1World);
+        this.setSize(0.9f, 1.3f);
+	}
+
+}
