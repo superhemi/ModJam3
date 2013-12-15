c11fce339b9ddc86b87f3ccb8e246662af051905
superhemi <hemisuperhemi@yahoo.com>
2013-12-15 15:14:59 -0500

    update

--- common/com/superhemi/PirateCraft/client/renderer/entity/RenderPirate.java
+++ common/com/superhemi/PirateCraft/client/renderer/entity/RenderPirate.java
@@ -1,51 +1,50 @@
 package com.superhemi.PirateCraft.client.renderer.entity;
 
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+
+import org.lwjgl.opengl.GL11;
+
 import com.superhemi.PirateCraft.client.model.ModelPirate;
 import com.superhemi.PirateCraft.entity.EntityPirate;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.renderer.entity.RenderLiving;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.ResourceLocation;
 
 @SideOnly(Side.CLIENT)
-public class RenderPirate extends RenderLiving
+public class RenderPirate extends Render
 {
 
 	private static final ResourceLocation pirateTextures  = new ResourceLocation("textures/entity/ModelPirate.png");
 	 
 	protected ModelPirate model;
 	
-	public RenderPirate(ModelBase par1ModelBase, float par2) 
+	public RenderPirate() 
 	{
-		super(par1ModelBase, par2);
-		model = ((ModelPirate)mainModel);
+		shadowSize = 0.5f;
+		model = new ModelPirate();
 	}
 	
-	public void PirateRender(EntityPirate entity, double par2, double par4, double par6, float par8, float par9){
-	 
-        super.doRenderLiving(entity, par2, par4, par6, par8, par9);
+	public void renderPirate(EntityPirate pirate, double x, double y, double z, float yaw, float partialTickTime)
+	{
+		GL11.glPushMatrix();
+		GL11.glTranslatef((float)x, (float)y, (float)z);
+		GL11.glRotatef(180.0F - yaw, 0.0F, 1.0F, 0.0F);
+		GL11.glScalef(-1.0F, -1.0F, 1.0F);
+		
+		func_110777_b(pirate);
+		
+		model.render(pirate, 0.0F, 0.0F, -0.0F, 0.0F, 0.0625F);
+		
+		GL11.glPopMatrix();
     } 
 
-    public void doRenderLiving(EntityLiving par1EntityLiving, double par2, double par4, double par6, float par8, float par9)
-    {
-        super.doRenderLiving(par1EntityLiving, par2, par4, par6, par8, par9);
-    }
-
-    public void doRender(Entity par1Entity, double par2, double par4, double par6, float par8, float par9)
-    {
-    PirateRender((EntityPirate)par1Entity, par2, par4, par6, par8, par9);
-    }
- 
-    @Override
-    protected int shouldRenderPass (EntityLivingBase par1EntityLiving, int par2, float par3)
-    {
-        return this.shouldRenderPass((EntityPirate) par1EntityLiving, par2, par3);
-    }
+	@Override
+	public void doRender(Entity entity, double x, double y, double z, float yaw, float partialTickTime)
+	{
+		this.renderPirate((EntityPirate)entity, x, y, z, yaw, partialTickTime);
+	}
 	@Override
 	protected ResourceLocation getEntityTexture(Entity par1Entity)
 	{
