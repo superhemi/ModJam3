ec0544f690aac8b0d1e44f5d1e4ccb602156a657
superhemi <hemisuperhemi@yahoo.com>
2013-12-16 19:50:52 -0500

    update and taking a break

--- common/com/superhemi/PirateCraft/core/proxy/CommonProxy.java
+++ common/com/superhemi/PirateCraft/core/proxy/CommonProxy.java
@@ -1,8 +1,11 @@
 package com.superhemi.PirateCraft.core.proxy;
 
+import java.util.Random;
+
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 
 import com.superhemi.PirateCraft.BaseForgePC;
@@ -11,10 +14,13 @@ import com.superhemi.PirateCraft.core.handlers.ServerTickHandler;
 import com.superhemi.PirateCraft.creativetab.CreativeTabPC;
 import com.superhemi.PirateCraft.entity.Entities;
 import com.superhemi.PirateCraft.entity.EntityPirate;
+import com.superhemi.PirateCraft.gen.ShipWorldGenerator;
 import com.superhemi.PirateCraft.item.ModItems;
 import com.superhemi.PirateCraft.lib.Reference;
 
+import cpw.mods.fml.common.IWorldGenerator;
 import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
@@ -53,8 +59,7 @@ public class CommonProxy {
          EntityRegistry.addSpawn(EntityPirate.class, 8000, 10, 20, EnumCreatureType.monster, BiomeGenBase.plains);
          EntityRegistry.addSpawn(EntityPirate.class, 25, 7, 15, EnumCreatureType.monster, BiomeGenBase.ocean);
          EntityList.addMapping(EntityPirate.class, "Pirate", 101, 14342901, 8026845);
-         LanguageRegistry.instance().addStringLocalization("entity.Pirate.name", "Pirate");
-            
+         LanguageRegistry.instance().addStringLocalization("entity.Pirate.name", "Pirate");            
      }
      
      public void initRecipes()
@@ -66,4 +71,15 @@ public class CommonProxy {
 	{
 		TickRegistry.registerTickHandler(new ServerTickHandler(), Side.SERVER);
 	}
+	
+	public void generateSurface(World world, Random rand, int y, int z)
+	{
+        for(int k = 0; k < 100; k++)
+        {
+            int RandPosX = y + rand.nextInt(16);
+            int RandPosY = rand.nextInt(128);
+            int RandPosZ = z + rand.nextInt(16);
+            (new ShipWorldGenerator()).generate(world, rand, RandPosX, RandPosY, RandPosZ);
+        }
+    }
 }
